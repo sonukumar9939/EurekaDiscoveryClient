770145a0c69402085d374ada6a01ae4ce30a35d9
sonukumar9939 <sonukumar.75421@gmail.com>
2020-04-19 02:23:32 +0530

    Eureka Discovery Client

---
+++ src/main/java/com/api/gateway/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -0,0 +1,13 @@
+package com.api.gateway.serviceregistrationanddiscoveryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistrationAndDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryClientApplication.class, args);
+	}
+
+}
