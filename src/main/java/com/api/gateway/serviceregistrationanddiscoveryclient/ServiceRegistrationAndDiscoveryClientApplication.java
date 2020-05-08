af85b110991d80c9cffd3058568f2ff3ab55aeae
sonukumar9939 <sonukumar.75421@gmail.com>
2020-05-08 21:12:19 +0530

    Added Eureka Peer to Peer and application.yaml

--- src/main/java/com/api/gateway/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
+++ src/main/java/com/api/gateway/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -2,8 +2,10 @@ package com.api.gateway.serviceregistrationanddiscoveryclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistrationAndDiscoveryClientApplication {
 
 	public static void main(String[] args) {
